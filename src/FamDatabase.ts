dc79f82d15ad6e8c70f280a79c82dddbd8e5f811
xiaolenz-cyber <xiaolenz@gmail.com>
2026-09-09 21:39:30 +0700

    Update FamDatabase.ts

--- src/FamDatabase.ts
+++ src/FamDatabase.ts
@@ -4814,7 +4814,7 @@ const famDatabase: CardMap = {
         img: "mtrd", rarity:5, evo: 7,
     },
         41808: {
-        name: "Gemini II", stats: [18000, 18585, 15050, 15409, 17956],
+        name: "Artificial", stats: [18000, 18585, 15050, 15409, 17956],
         skills: [10198, 10199],
         img: "gem", rarity: 5, evo: 8,
         fullName: "Artificial, Gemini II"
