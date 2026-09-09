17853d47df4a594c9c54d64f87a24edf6476b81f
xiaolenz-cyber <xiaolenz@gmail.com>
2026-09-09 19:04:33 +0700

    Update SkillDatabase.ts

--- src/SkillDatabase.ts
+++ src/SkillDatabase.ts
@@ -8383,6 +8383,6 @@ const SkillDatabase: SkillMap = {
         name: "Synthetic Blade", type: 2, func: 3, calc: 8,
         args: [3.5, 2, 1],
         range: 16, prob: 100, ward: 1,
-        desc: "Atk+Wis 2.5x for 3 random enemy ignore position, Paralysis Effect, prob 50%"
+        desc: "Atk+Wis 3.5x for 3 random enemy ignore position, Paralysis Effect, prob 50%"
     },
 };
